c5a14d30b18381697b8053604a70a547f3c25710
KuznetsovArtem95 <kyziaradiominer@gmail.com>
2024-12-18 14:24:01 +0500

    module_5_3_dorabotka

--- pythonProject_CycleFor/module_5_3.py
+++ pythonProject_CycleFor/module_5_3.py
@@ -15,32 +15,42 @@ class House:
         return f'Название:{self.name}, кол-во этажей:{self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
 
     def __add__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        elif isinstance(value, House):
+            return House(self.name, self.number_of_floors + value.number_of_floors)
 
-    def __iadd__(self, other):
-        return self + other
+    def __iadd__(self, value):
+        return self.__add__(value)
 
-    def __radd__(self, other):
-        return self + other
+    def __radd__(self, value):
+        return self.__add__(value)
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
